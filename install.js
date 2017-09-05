443ffe0cd2555e1d33403e4e589a0e030fb91cf4
Mukesh Soni <mukeshsoni@gmail.com>
2017-09-05 07:30:08 +0530

    cleaned up the install script

--- install.js
+++ install.js
@@ -16,40 +16,83 @@ var fs = require('fs-extra')
 var path = require('path')
 var exec = require('child_process').exec
 var pify = require('pify')
+// TODO - remove ncp
 var ncp = require('ncp').ncp
 var recursivelyCopy = pify(ncp)
+var promisifiedExec = pify(exec)
 
 var reactpenFolder = './.reactpen'
 
 // create .reactpen folder if it doesn't exist
-if (!fs.existsSync(reactpenFolder)) {
-    fs.mkdirSync(reactpenFolder)
+function createReactpenFolder() {
+    if (!fs.existsSync(reactpenFolder)) {
+        return fs.mkdirSync(reactpenFolder)
+    } else {
+        return Promise.resolve({})
+    }
 }
 
 // remove any existing stuff from .reactpen, if any
-fs.remove(reactpenFolder + '/*').catch(console.error)
+function emptyReactpenFolder() {
+    return fs.remove(reactpenFolder + '/*')
+}
 
-// copy stuff we need
-const filesToCopy = [
-    'index.html',
-    'meta_data_generator.js',
-    'jspm.config.js',
-    'package.json',
-    'main.js',
-    'src/*'
-]
+function copyNeededFiles() {
+    // copy stuff we need
+    const filesToCopy = [
+        'index.html',
+        'meta_data_generator.js',
+        'belt.js',
+        'jspm.config.js',
+        'package.json',
+        'main.js',
+        'src'
+    ]
 
-filesToCopy.forEach(file => {
-    fs.copy(file, reactpenFolder + '/' + file)
-})
+    return Promise.all(
+        filesToCopy.map(file => {
+            return fs.copy(file, reactpenFolder + '/' + file)
+        })
+    )
+}
 
-// cd .reactpen
-process.chdir(reactpenFolder)
-fs
-    .readdir(reactpenFolder)
-    .then(files => files.forEach(console.log))
-    .catch(console.error)
-// npm install
+function gotoReactpenFolder() {
+    // process.chdir is synchronous. We need async for our purpose
+    return new Promise(resolve => {
+        process.chdir(reactpenFolder)
+        resolve()
+    })
+}
 
-var promisifiedExec = pify(exec)
-promisifiedExec('npm i').then(() => promisifiedExec('npm run build'))
+function installNpmModules() {
+    return promisifiedExec('npm install')
+}
+
+function installJspmModules() {
+    return promisifiedExec('npm run jspm')
+}
+
+function generateMetaFile() {
+    return promisifiedExec('npm run docgen')
+}
+
+function pifyLog(msg) {
+    console.log(msg)
+    return Promise.resolve({})
+}
+
+pifyLog('Creating .reactpen  folder')
+    .then(createReactpenFolder)
+    .then(pifyLog.bind(null, 'Emptying .reactpen folder'))
+    .then(emptyReactpenFolder)
+    .then(pifyLog.bind(null, 'copying needed files'))
+    .then(copyNeededFiles)
+    .then(pifyLog.bind(null, 'going inside .reactpen folder'))
+    .then(gotoReactpenFolder)
+    .then(pifyLog.bind(null, 'installing npm modules'))
+    .then(installNpmModules)
+    .then(pifyLog.bind(null, 'installing jspm modules'))
+    .then(installJspmModules)
+    .then(pifyLog.bind(null, 'generating meta file for components'))
+    .then(generateMetaFile)
+    .catch(e => console.log('Error creating reactpen stuff', e))
