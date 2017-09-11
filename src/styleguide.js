43e35fe299ea4fab41254fdf1f6a619ade273c4e
Mukesh Soni <mukeshsoni@gmail.com>
2017-09-11 16:14:18 +0530

    renamed service worker file

---
+++ src/styleguide.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class StyleGuide extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <div />
+                <div />
+            </div>
+        )
+    }
+}
